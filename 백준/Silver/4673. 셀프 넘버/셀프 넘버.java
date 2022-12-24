27b15f3a2b1ed00e5da0b2e44296fd45bb58db36
Minjeong Jang <leomj.dev@gmail.com>
2022-12-24 13:16:01 +0900

    [Silver V] Title: 셀프 넘버, Time: 160 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -16,9 +16,6 @@ public class Main {
         for(int i = 1; i <= 10000; i++) {
             int j = d(i);
             if(j <= 10000) array[j] = true;
-        }
-
-        for(int i = 1; i <= 10000; i ++) {
             if(array[i] == false) System.out.println(i);
         }
     }
