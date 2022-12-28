f77f6cdc22811e318c2fb96ceb2f02810c14e473
Minjeong Jang <leomj.dev@gmail.com>
2022-12-28 11:45:48 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 228 ms, Memory: 18276 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -7,22 +7,8 @@ public class Main {
 
         sc.close();
 
-        int array[] = new int[26];
-
-        for(int i = 0; i < array.length; i++) {
-            array[i] = -1;
-        }
-
-        for(int i = 0; i < s.length(); i++) {
-            char result = s.charAt(i);
-
-            if(array[result - 'a'] == -1) {
-                array[result - 'a'] = i;
-            }
-        }
-
-        for(int val: array) {
-            System.out.print(val + " ");
+        for(char c = 'a'; c <= 'z'; c++) {
+            System.out.print(s.indexOf(c) + " ");
         }
     }
 }
