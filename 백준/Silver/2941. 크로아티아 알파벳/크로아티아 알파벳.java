76ac8f8d6e3eaddb733d4d7f56b02446f986249f
Minjeong Jang <leomj.dev@gmail.com>
2023-01-03 09:35:19 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 200 ms, Memory: 17592 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -4,27 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
+        String[] croatian = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
         sc.close();
 
-        int count = 0;
-        char[] chars = word.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            count++;
-        }
+        for(int i=0; i < croatian.length ;i++)
+            word = word.replaceAll(croatian[i], "*");
 
-        for(int j = 0; j < chars.length-1; j++) {
-            if((chars[j] + "" + chars[j+1]).equals("c=") || (chars[j] + "" + chars[j+1]).equals("c-") || (chars[j] + "" + chars[j+1]).equals("d-") || (chars[j] + "" + chars[j+1]).equals("lj") || (chars[j] + "" + chars[j+1]).equals("nj") || (chars[j] + "" + chars[j+1]).equals("s=") || (chars[j] + "" + chars[j+1]).equals("z=")) {
-                count--;
-            }
-        }
-
-        for(int j = 0; j < chars.length-2; j++) {
-            if((chars[j] + "" + chars[j+1] + "" + chars[j+2]).equals("dz=")) {
-                count--;
-            }
-        }
-
-        System.out.println(count);
+        System.out.println(word.length());
     }
 }
