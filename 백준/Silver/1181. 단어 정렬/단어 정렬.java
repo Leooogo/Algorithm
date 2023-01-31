572915aa9d4fdb650e713887087a7d4b284307d4
Minjeong Jang <leomj.dev@gmail.com>
2023-01-31 13:03:15 +0900

    [Silver V] Title: 단어 정렬, Time: 660 ms, Memory: 67300 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String array[] = new String[n];
+        StringBuilder sb = new StringBuilder();
+        sc.nextLine();
+
+        for(int i = 0; i < n; i++) {
+            array[i] = sc.nextLine();
+        }
+
+        sc.close();
+        Arrays.sort(array, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        sb.append(array[0]).append("\n");
+
+        for(int i = 1; i < n; i++) {
+            if(!array[i].equals(array[i-1])) {
+                sb.append(array[i]).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
